222073c6e1a948b4a28bfd666e4af30520202b94
smc2315 <74086927+smc2315@users.noreply.github.com>
2023-01-17 21:45:31 +0900

    [level 3] Title: 징검다리 건너기, Time: 44.49 ms, Memory: 12.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/64062. 징검다리 건너기/징검다리 건너기.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <set>
+using namespace std;
+
+
+int solution(vector<int> stones, int k) {
+    multiset<int> s;
+    int mmin = 1e9;
+    for(int i=0;i<k-1;i++){
+        s.insert(stones[i]);
+    }
+    for(int i= k-1;i<stones.size();i++){
+        s.insert(stones[i]);
+        auto it = s.end();
+        --it;
+        mmin = min(mmin,*it);
+        s.erase(s.find(stones[i-(k-1)]));
+    }
+    return mmin;
+}
