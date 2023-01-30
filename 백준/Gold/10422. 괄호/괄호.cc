91dd8fa1a6528c1646bf3e9e1e9760d66afaab29
smc2315 <74086927+smc2315@users.noreply.github.com>
2023-01-30 11:47:56 +0900

    [Gold IV] Title: 괄호, Time: 8 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/10422. 괄호/괄호.cc
+++ 백준/Gold/10422. 괄호/괄호.cc
@@ -12,8 +12,8 @@ int main() {
 	dp[0] = 1;
 	
 	dp[2] = 1;
-	for (int i = 3; i < 5001; i++) {
-		for (int j = 2; j <= i; j++) {
+	for (int i = 4; i < 5001; i+=2) {
+		for (int j = 2; j <= i; j+=2) {
 			dp[i] += (dp[j - 2] * dp[i - j])%1000000007;
 
 		}
