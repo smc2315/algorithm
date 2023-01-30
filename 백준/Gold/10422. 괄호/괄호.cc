70a1a9747d64a1d98343f3d1465ac5e44f2a6738
smc2315 <74086927+smc2315@users.noreply.github.com>
2023-01-30 11:47:37 +0900

    [Gold IV] Title: 괄호, Time: 24 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Gold/10422. 괄호/괄호.cc
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<cstring>
+
+using namespace std;
+int T;
+long long dp[5001];
+
+int main() {
+	cin >> T;
+	dp[0] = 1;
+	
+	dp[2] = 1;
+	for (int i = 3; i < 5001; i++) {
+		for (int j = 2; j <= i; j++) {
+			dp[i] += (dp[j - 2] * dp[i - j])%1000000007;
+
+		}
+        dp[i]%=1000000007;
+		
+	}
+	
+	while (T--) {
+		int N;
+		cin >> N;
+		cout << dp[N] << endl;
+
+	}
+	return 0;
+}
