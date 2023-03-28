6e79686d23bc34cabbda8abdc18179e6af5f5f56
Yongtae Choi <74086927+smc2315@users.noreply.github.com>
2023-03-28 14:52:26 +0900

    [Silver III] Title: 1로 만들기, Time: 80 ms, Memory: 11512 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,30 +1,19 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
-    static int[] memo;
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        memo = new int[N + 1];
-        Arrays.fill(memo, -1);
-        System.out.println(recur(N));
+        System.out.println(recur(N,0));
     }
 
-    static int recur(int N) {
-        if (N < 2) {
-            return 0;
+    static int recur(int N, int count){
+        if(N<2){
+            return count;
         }
-        if (memo[N] != -1) {
-            return memo[N];
-        }
-        int result = recur(N / 2) + 1 + (N % 2);
-        result = Math.min(result, recur(N / 3) + 1 + (N % 3));
-        memo[N] = result;
-        return result;
+        return Math.min(recur(N/2, count+1+(N%2)), recur(N/3, count+1+(N%3)));
     }
 }
