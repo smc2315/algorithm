3f895c0aad47a2dada65fb1aba998ca764d20724
Yongtae Choi <74086927+smc2315@users.noreply.github.com>
2023-06-08 12:49:33 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -1,36 +1,31 @@
 #include <iostream>
-#include <stack>
 #include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <queue>
 using namespace std;
 
-int main()
-{
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	int N;
-	cin >> N;
+int T;
+int N;
+int dp[12];
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	cin >> T;
+
+	dp[0] = 0;
+	dp[1] = 1;
+	dp[2] = 2;
+	dp[3] = 4;
 	
-	while (N--) {
-		int dp[11] = { 0, };
-		dp[1] = 1;
-		dp[2] = 2;
-		dp[3] = 4;
-		int M;
-		cin >> M;
-		if (M == 1)
-			cout << 1<<"\n";
-		else if (M == 2)
-			cout << 2<<"\n";
-		else {
-			for (int i = 4; i <= M; i++) {
-				dp[i] += dp[i - 1];
-				dp[i] += dp[i - 2];
-				dp[i] += dp[i - 3];
-			}
-			cout << dp[M]<<"\n";
-		}
+	for (int i = 4; i < 12; i++) {
+		dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+		
+	}
+
+	while (T--) {
+		cin >> N;
+		cout << dp[N] << endl;
 	}
 	return 0;
-
-
 }
