50fe0c6d127cfe087ade97bd4ccc6916084e8d15
Yongtae Choi <74086927+smc2315@users.noreply.github.com>
2023-07-21 11:13:12 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 5940 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.cc
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -1,34 +1,22 @@
 #include <iostream>
-#include <string>
 #include <algorithm>
-#include <vector>
-
 using namespace std;
-int LCS[1001][1001];
 
-int main()
-{
+string s1,s2;
 
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+int dp[1001][1001];
+int main(){
+    cin>>s1>>s2;
 
-	
-	string s1, s2;
-	cin >> s1 >> s2;
-
-
-	
-	for (int i = 0; i < s1.length(); i++) {
+    for (int i = 0; i < s1.length(); i++) {
 		for (int j = 0; j < s2.length(); j++) {
 			if (s1[i] == s2[j]) {
-				LCS[i + 1][j + 1] = LCS[i][j] + 1;
+				dp[i + 1][j + 1] = dp[i][j] + 1;
 			}
 			else {
-				LCS[i + 1][j + 1] = max(LCS[i][j + 1], LCS[i+1][j]);
+				dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i+1][j]);
 			}
 		}
 	}
-	cout << LCS[s1.length()][s2.length()];
-	return 0;
+	cout << dp[s1.length()][s2.length()];
 }
