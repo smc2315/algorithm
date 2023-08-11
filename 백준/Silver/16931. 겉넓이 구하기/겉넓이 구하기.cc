a653712957c9daf708275752d6836dd678b59c3c
Yongtae Choi <74086927+smc2315@users.noreply.github.com>
2023-08-11 12:36:31 +0900

    [Silver II] Title: 겉넓이 구하기, Time: 4 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Silver/16931. 겉넓이 구하기/겉넓이 구하기.cc
+++ 백준/Silver/16931. 겉넓이 구하기/겉넓이 구하기.cc
@@ -17,20 +17,14 @@ int main() {
     for(int i=1;i<=N;i++){
         for(int j=1;j<=M;j++){
             if(arr[i][j] > arr[i][j-1]) {
-                ans += arr[i][j] - arr[i][j-1];
-            }
-            if(arr[i][j] > arr[i][j+1]) {
-                ans += arr[i][j] - arr[i][j+1];
+                ans += (arr[i][j] - arr[i][j-1]) * 2;
             }
         }
     }
     for(int j=1;j<=M;j++){
         for(int i=1;i<=N;i++){
             if(arr[i][j] > arr[i-1][j]) {
-                ans += arr[i][j] - arr[i-1][j];
-            }
-            if(arr[i][j] > arr[i+1][j]) {
-                ans += arr[i][j] - arr[i+1][j];
+                ans += (arr[i][j] - arr[i-1][j])*2;
             }
         }
     }
