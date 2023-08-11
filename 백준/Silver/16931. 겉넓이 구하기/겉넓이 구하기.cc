14148a4a0f0b861f7fdbc2f2ef804738ee8f9721
Yongtae Choi <74086927+smc2315@users.noreply.github.com>
2023-08-11 11:22:34 +0900

    [Silver II] Title: 겉넓이 구하기, Time: 4 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Silver/16931. 겉넓이 구하기/겉넓이 구하기.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N,M;
+int arr[102][102];
+int ans;
+int main() {
+    cin>>N>>M;
+    for(int i=1;i<=N;i++){
+        for(int j=1;j<=M;j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    for(int i=1;i<=N;i++){
+        for(int j=1;j<=M;j++){
+            if(arr[i][j] > arr[i][j-1]) {
+                ans += arr[i][j] - arr[i][j-1];
+            }
+            if(arr[i][j] > arr[i][j+1]) {
+                ans += arr[i][j] - arr[i][j+1];
+            }
+        }
+    }
+    for(int j=1;j<=M;j++){
+        for(int i=1;i<=N;i++){
+            if(arr[i][j] > arr[i-1][j]) {
+                ans += arr[i][j] - arr[i-1][j];
+            }
+            if(arr[i][j] > arr[i+1][j]) {
+                ans += arr[i][j] - arr[i+1][j];
+            }
+        }
+    }
+    ans += N*M*2;
+    cout<<ans;
+}
