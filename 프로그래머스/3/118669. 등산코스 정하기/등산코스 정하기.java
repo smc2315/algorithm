903a155c996170c86cde882085df6cb81cb59695
Yongtae Choi <74086927+smc2315@users.noreply.github.com>
2023-11-14 15:28:38 +0900

    [level 3] Title: 등산코스 정하기, Time: 143.78 ms, Memory: 157 MB -BaekjoonHub

---
+++ 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.java
@@ -0,0 +1,100 @@
+import java.util.*;
+
+class Node implements Comparable<Node> {
+    int index;
+    int intensity;
+
+    public Node(int index, int intensity) {
+        this.index = index;
+        this.intensity = intensity;
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        return this.intensity - o.intensity;
+    }
+}
+
+class Solution {
+    public int[] solution(int n, int[][] paths, int[] gates, int[] summits) {
+        ArrayList<ArrayList<Node>> graph = createGraph(n, paths);
+        HashMap<Integer, Boolean> summitMap = createSummitMap(summits);
+        HashMap<Integer, Boolean> gateMap = createGateMap(summits);
+
+        int[] intensities = dijkstra(n, graph, gates, gateMap, summitMap);
+        
+        Arrays.sort(summits);
+        int index = -1;
+        int minIntensity = Integer.MAX_VALUE;
+        for (int summit : summits) {
+            if (intensities[summit] < minIntensity) {
+                minIntensity = intensities[summit];
+                index = summit;
+            }
+        }
+        return new int[]{index, minIntensity};
+    }
+
+    private static HashMap<Integer, Boolean> createGateMap(int[] gates) {
+        HashMap<Integer, Boolean> gateMap = new HashMap<>();
+        for (int gate : gates) {
+            gateMap.put(gate, true);
+        }
+        return gateMap;
+    }
+
+    private static HashMap<Integer, Boolean> createSummitMap(int[] summits) {
+        HashMap<Integer, Boolean> summitMap = new HashMap<>();
+        for (int summit : summits) {
+            summitMap.put(summit, true);
+        }
+        return summitMap;
+    }
+
+    private ArrayList<ArrayList<Node>> createGraph(int n, int[][] paths) {
+        ArrayList<ArrayList<Node>> graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        for (int[] p : paths) {
+            graph.get(p[0]).add(new Node(p[1], p[2]));
+            graph.get(p[1]).add(new Node(p[0], p[2]));
+        }
+        return graph;
+    }
+    
+    private int[] dijkstra(int n, ArrayList<ArrayList<Node>> graph, int[]gates, HashMap<Integer, Boolean> summitMap, HashMap<Integer, Boolean> gateMap) {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+
+        int[] intensities = new int[n + 1];
+        Arrays.fill(intensities, Integer.MAX_VALUE);
+
+        for (int gate : gates) {
+            pq.offer(new Node(gate, 0));
+            intensities[gate] = 0;
+        }
+
+        while (!pq.isEmpty()) {
+            Node now = pq.poll();
+
+            if (gateMap.containsKey(now.index) || summitMap.containsKey(now.index)) {
+                continue;
+            }
+
+            if (intensities[now.index] < now.intensity) {
+                continue;
+            }
+
+            for (Node next : graph.get(now.index)) {
+                int intensity = (next.intensity == Integer.MAX_VALUE) ? now.intensity : Math.max(next.intensity, now.intensity);
+                if (intensities[next.index] > intensity) {
+                    intensities[next.index] = intensity;
+                    pq.offer(new Node(next.index, intensity));
+                }
+            }
+        }
+        return intensities;
+    }
+}
+
+
