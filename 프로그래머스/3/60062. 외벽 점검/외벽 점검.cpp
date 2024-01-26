20b5f535b09af880062dd1e5cbb2e2020e489d09
Yongtae Choi <74086927+smc2315@users.noreply.github.com>
2024-01-26 14:00:59 +0900

    [level 3] Title: 외벽 점검, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/3/60062. 외벽 점검/외벽 점검.cpp
@@ -0,0 +1,43 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int n, vector<int> weak, vector<int> dist) {
+    int answer = 1e8;
+    int w_size = weak.size();
+    
+    for(int i = 0; i < w_size - 1; i++) {
+        weak.push_back(weak[i] + n);
+    }
+    
+    sort(dist.begin(), dist.end()); 
+    
+    do {
+        for(int i = 0; i < w_size; i++) {
+            int start = weak[i];
+            int end = weak[i + w_size - 1];
+            
+            for(int j = 0; j < dist.size(); j++) {
+                start += dist[j]; 
+                
+                if(start >= end) { 
+                    answer = min(answer, j + 1);
+                    break;
+                }
+                
+                for(int z = i + 1; z < i + w_size; z++) {
+                    if(weak[z] > start) {
+                        start = weak[z];
+                        break;
+                    }
+                }
+            }
+        }
+    } while(next_permutation(dist.begin(), dist.end()));
+    
+    if(answer == 1e8) 
+        return -1;
+    
+    return answer;
+}
